7cee84f04ae9952831104dca4e9154c09bda490f
aakash018 <aakashkhanal015@gmail.com>
2020-05-19 18:03:58 +0545

    Error Phase

--- routes/characters.js
+++ routes/characters.js
@@ -13,8 +13,20 @@ router.get("/new", (req, res) => {
 });
 
 // Creat
-router.post("/", (req, res) => {
-  res.send(req.body.name);
+router.post("/", async (req, res) => {
+  const character = new Character({
+    name: req.body.name,
+  });
+  try {
+    const newCharacter = await character.save();
+    // res.redirect(`characters/${newCharacter.id}`)
+    res.redirect("characters");
+  } catch {
+    res.render("characters/new", {
+      character: character,
+      errorMessage: "Error Creating",
+    });
+  }
 });
 
 module.exports = router;
